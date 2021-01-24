e7c9916f06347f3add4019852dfd9be407f568be
John Holliman <github@jhh3.com>
2021-01-24 13:21:33 -0800

    Add inline editor support

--- django_admin_json_editor/static/django_admin_json_editor/editor.js
+++ django_admin_json_editor/static/django_admin_json_editor/editor.js
@@ -1,46 +1,33 @@
-var editor = {}
-var $ = django.jQuery
-var enable_json_editor = function(element){
+var editors = {};
+var $ = django.jQuery;
+
+const injectJSONEditor = (element) => {
   var id = element.id
   if (id.indexOf('__prefix__') === -1) { // activate editor only if it is not an inline template row
     var textarea_id = "id_"+element.id.replace('_editor', '')
-    console.log("XXXXXXXXXXX");
-    console.log(element.getAttribute('data-schema'));
-    var schema = JSON.parse(element.getAttribute('data-schema'))
     var data = JSON.parse(element.getAttribute('data-data'))
-    var options = {
-        theme: "bootstrap3",
-        iconlib: "fontawesome4",
-        schema: schema,
-    };
-    editor[id] = new JSONEditor(element, options);
-    JSONEditor.plugins.sceditor.emoticonsEnabled = element.getAttribute("data-sceditor");
-    editor[id].on('change', function () {
-        var errors = editor[id].validate();
-        if (errors.length) {
-            console.log(errors);
-        }
-        else {
-            var json = editor[id].getValue();
-            $('#'+textarea_id)[0].value = JSON.stringify(json);
-        }
+    var options = JSON.parse(element.getAttribute('data-options'))
+
+    editors[id] = new JSONEditor(element, options);
+    editors[id].on('change', function () {
+      var errors = editors[id].validate();
+      if (errors.length) {
+        console.log(errors);
+      } else {
+        var json = editors[id].getValue();
+        $('#'+textarea_id)[0].value = JSON.stringify(json);
+      }
     });
+
     if (data != null) {
-      editor[id].setValue(data);
+      editors[id].setValue(data);
     }
-
   }
-}
+};
 
-$(document).ready(function() {
-  $(document).find("[id$=_editor]").each(function(){
-    enable_json_editor(this);      
-  });  
-});
+// Inject editors in the normal detail view and in inlines
 
-$(document).on('formset:added', function(event, $row, formsetName) {
-  $row.find("[id$=_editor]").each(function(){
-    enable_json_editor(this);      
-  });  
+const searchAndInject = () => $(document).find("[id$=_editor]").each(() => injectJSONEditor(this));  
 
-});
+$(document).ready(searchAndInject);
+$(document).on('formset:added', searchAndInject);
