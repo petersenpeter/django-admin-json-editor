ff91095f5a9767ee12576192261c1230f5e33bf0
John Holliman <github@jhh3.com>
2021-02-01 18:48:58 -0800

    Try use window load event

--- django_admin_json_editor/static/django_admin_json_editor/editor.js
+++ django_admin_json_editor/static/django_admin_json_editor/editor.js
@@ -1,45 +1,48 @@
-var editors = {};
-var $ = django.jQuery;
+const onLoad = $ => {
+  var editors = {};
 
-const htmlDecode = (input) => {
-  var doc = new DOMParser().parseFromString(input, "text/html");
-  return doc.documentElement.textContent;
-}
-
-const injectJSONEditor = (element) => {
-  var id = element.id
-  if (id.indexOf('__prefix__') === -1) { // activate editor only if it is not an inline template row
-    var textarea_id = "id_"+element.id.replace('_editor', '');
-    var data = JSON.parse(htmlDecode(element.getAttribute('data-data')));
-    var options = JSON.parse(element.getAttribute('data-options'));
-
-    editors[id] = new JSONEditor(element, options);
-    editors[id].on('change', function () {
-      var errors = editors[id].validate();
-      if (errors.length) {
-        console.log(errors);
-      } else {
-        var json = editors[id].getValue();
-        $('#'+textarea_id)[0].value = JSON.stringify(json);
-      }
-    });
-
-    if (data != null) {
-      editors[id].setValue(data);
-    }
+  const htmlDecode = (input) => {
+    var doc = new DOMParser().parseFromString(input, "text/html");
+    return doc.documentElement.textContent;
   }
+
+  const injectJSONEditor = (element) => {
+    var id = element.id
+    if (id.indexOf('__prefix__') === -1) { // activate editor only if it is not an inline template row
+      var textarea_id = "id_"+element.id.replace('_editor', '');
+      var data = JSON.parse(htmlDecode(element.getAttribute('data-data')));
+      var options = JSON.parse(element.getAttribute('data-options'));
+
+      editors[id] = new JSONEditor(element, options);
+      editors[id].on('change', function () {
+        var errors = editors[id].validate();
+        if (errors.length) {
+          console.log(errors);
+        } else {
+          var json = editors[id].getValue();
+          $('#'+textarea_id)[0].value = JSON.stringify(json);
+        }
+      });
+
+      if (data != null) {
+        editors[id].setValue(data);
+      }
+    }
+  };
+
+  // Inject editors in the normal detail view and in inlines
+
+  $(document).ready(() => {
+    $(document).find("[id$=_editor]").each(function(){
+      injectJSONEditor(this);      
+    });  
+  });
+
+  $(document).on('formset:added', (event, $row, formsetName) => {
+    $row.find("[id$=_editor]").each(function(){
+      injectJSONEditor(this);      
+    });  
+  });
 };
 
-// Inject editors in the normal detail view and in inlines
-
-$(document).ready(() => {
-  $(document).find("[id$=_editor]").each(function(){
-    injectJSONEditor(this);      
-  });  
-});
-
-$(document).on('formset:added', (event, $row, formsetName) => {
-  $row.find("[id$=_editor]").each(function(){
-    injectJSONEditor(this);      
-  });  
-});
+window.addEventListener("load", () => onLoad(django.jQuery));
